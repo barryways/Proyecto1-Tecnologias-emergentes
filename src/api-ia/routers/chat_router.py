063ee053c5b3aaa2ec17b4d65bec747fe90840fa
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2026-04-04 23:10:47 -0600

    feat(api-ai): Change claude to open ai for ask

--- src/api-ia/routers/chat_router.py
+++ src/api-ia/routers/chat_router.py
@@ -1,34 +1,35 @@
 import logging
 from security import verify_api_key
 from fastapi import APIRouter, Depends
-from services.claude_service import ask_tutor
-from models.chat_schemas import ChatRequest, ChatResponse
-from dependencies import get_collection_tutor_advanced_programming
+from services.chat_service import ask_tutor
 from models.api_response import ApiResponse
+from models.chat_schemas import ChatRequest, ChatResponse
+
 
 router = APIRouter(prefix="/chat", tags=["Chat"])
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 @router.post("/ask", response_model=ApiResponse[ChatResponse], dependencies=[Depends(verify_api_key)])
-def chat_completion(request: ChatRequest, collection=Depends(get_collection_tutor_advanced_programming)):
+def chat_completion(request: ChatRequest):
     try:
         logger.info('❗ en - <chat_completion>')
-        answer = ask_tutor(request.message, collection)
-        logger.info('✅ respuesta generada')
+        answer = ask_tutor(request.message)
         return ApiResponse.ok(
             data=ChatResponse(content=answer),
             message="Respuesta generada exitosamente"
         )
     except ValueError as e:
-        logger.error(f'❌ error en <chat_completion>: {str(e)}')
         return ApiResponse.error(
             message=f"Error, motivo: {str(e)}",
-            code=422
+            code=422,
+            method='chat_completion',
+            e=e
         )
     except Exception as e:
-        logger.error(f'❌ error en <chat_completion>: {str(e)}')
         return ApiResponse.error(
-            message=f"Error al procesar la pregunta",
-            code=500
+            message=f"Falla al generar la respuesta",
+            code=500,
+            method='chat_completion',
+            e=e
         )
